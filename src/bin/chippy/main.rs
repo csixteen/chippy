96f38a813c6ea927dc54530ec2f3d0639c81d52d
Pedro Rodrigues <csixteen@protonmail.com>
2021-04-24 21:09:55 +0200

    Preloaded sprites with font data

--- src/bin/chippy/main.rs
+++ src/bin/chippy/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, chippy!");
+extern crate clap;
+
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+use clap::{Arg, App};
+
+fn main() -> io::Result<()> {
+    let matches = App::new("CHIP-8 interpreter written in Rust.")
+                        .version("1.0.0")
+                        .author("Pedro Rodrigues <csixteen@protonmail.com>")
+                        .arg(Arg::with_name("file_name")
+                             .value_name("FILE")
+                             .help("CHIP-8 program source file.")
+                             .takes_value(true)
+                             .required(true))
+                        .get_matches();
+
+    let file_name = matches.value_of("file_name").unwrap();
+    let mut f = File::open(file_name)?;
+    let mut buffer = Vec::new();
+
+    f.read_to_end(&mut buffer)?;
+
+    Ok(())
 }
---
+++ src/opcodes.rs
@@ -0,0 +1,6 @@
+impl Chip8 {
+    pub fn execute_instruction(&mut self, opcode: u16) {
+        match opcode {
+        }
+    }
+}
