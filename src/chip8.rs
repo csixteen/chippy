0e5a67f1c6bd6f068e869cf162dca90bced48437
Pedro Rodrigues <csixteen@protonmail.com>
2021-04-26 20:25:01 +0200

    Adds LICENSE and a few more instructions and tests

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,3 +1,25 @@
+// MIT License
+//
+// Copyright (c) 2021 Pedro Rodrigues
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 use std::mem;
 
 const DISPLAY_WIDTH: usize  = 64;
@@ -147,7 +169,7 @@ mod tests {
         assert_eq!(0x202, c.pc);
         assert_eq!(0x1, c.v_reg[0x1]);
 
-        for i in 0..=15 {
+        for i in 0..=253 {
             c.fetch_decode_execute();
             assert_eq!(0x204, c.pc);
             assert_eq!(0x2 + (i as u8), c.v_reg[0x1]);
@@ -158,5 +180,20 @@ mod tests {
             c.fetch_decode_execute();
             assert_eq!(c.pc, 0x202);
         }
+
+        c.fetch_decode_execute();
+        assert_eq!(0x204, c.pc);
+        assert_eq!(0x0, c.v_reg[0x1]);
+
+        c.fetch_decode_execute();
+        assert_eq!(0x208, c.pc);
+
+        c.fetch_decode_execute();
+        assert_eq!(0x20A, c.pc);
+        assert_eq!(0x1, c.v_reg[0x1]);
+
+        c.fetch_decode_execute();
+        assert_eq!(0x20C, c.pc);
+        assert_eq!(0xFF, c.v_reg[0x2]);
     }
 }
--- src/opcodes.rs
+++ src/opcodes.rs
@@ -1,3 +1,25 @@
+// MIT License
+//
+// Copyright (c) 2021 Pedro Rodrigues
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 use crate::chip8::Chip8;
 
 impl Chip8 {
@@ -10,10 +32,10 @@ impl Chip8 {
     }
 
     fn get_8bit_value(&self, opcode: u16) -> u8 {
-        (opcode & 0xF) as u8
+        (opcode & 0xFF) as u8
     }
 
-    pub fn execute_instruction(&mut self, opcode: u16) -> u16 {
+    pub(crate) fn execute_instruction(&mut self, opcode: u16) -> u16 {
         let mut new_pc = self.pc + 2;
 
         match opcode {
@@ -23,6 +45,14 @@ impl Chip8 {
             0x1000..=0x1FFF => {
                 let addr: u16 = opcode & 0xFFF;
                 new_pc = addr;
+            },
+            // SE Vx, byte
+            0x3000..=0x3FFF => {
+                let vx = self.get_reg_x(opcode);
+                let value = self.get_8bit_value(opcode);
+                if self.v_reg[vx as usize] == value {
+                    new_pc += 2;
+                }
             },
             // LD Vx, byte
             0x6000..=0x6FFF => {
