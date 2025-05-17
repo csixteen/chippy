fee8d40000a7645f20c4e17324d3bf6cd161933b
Pedro Rodrigues <csixteen@protonmail.com>
2025-05-17 19:14:41 +0200

    nix-direnv and friends

---
+++ default.nix
@@ -0,0 +1,22 @@
+{ nixpkgs ? import <nixpkgs> { }}:
+
+let
+  rustOverlay = builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
+  pinnedPkgs = nixpkgs.fetchFromGitHub {
+    owner  = "NixOS";
+    repo   = "nixpkgs";
+    rev    = "ba8b70ee098bc5654c459d6a95dfc498b91ff858";
+    hash   = "sha256-IKKIXTSYJMmUtE+Kav5Rob8SgLPnfnq4Qu8LyT4gdqQ=";
+  };
+  pkgs = import pinnedPkgs {
+    overlays = [ (import rustOverlay) ];
+  };
+in
+  pkgs.mkShell {
+    buildInputs = with pkgs; [
+      rust-bin.stable.latest.default
+      rust-analyzer
+    ];
+
+    RUST_BACKTRACE = 1;
+  }
